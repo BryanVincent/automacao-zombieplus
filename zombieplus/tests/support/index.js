df0debf0328b06b3f04d1673f4b140f8176f1507
BryanVincent <bryanvincentrferreira@gmail.com>
2025-08-28 23:39:43 -0300

    Subindo funções para executar requisições e ajustes de falta do await

--- zombieplus/tests/support/index.js
+++ zombieplus/tests/support/index.js
@@ -3,6 +3,7 @@ const { Leads } = require('./actions/Leads')
 const { Login } =  require('./actions/Login')
 const { Movies } = require('./actions/Movies')
 const { Toast } = require('./actions/Components')
+const { Api } = require('./api')
 
 const test = base.extend({
     page: async ({page}, use) => {
@@ -12,6 +13,12 @@ const test = base.extend({
         context['movies'] = new Movies(page)
         context['toast'] = new Toast(page)
         
+        await use(context)
+    },
+    request: async({request}, use) => {
+        const context = request
+        context['api'] = new Api(request)
+        await context['api'].setToken()
         await use(context)
     }
 })
