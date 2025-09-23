dc88f897bb9f6a1cef4635a590f7e4bb448b752d
BryanVincent <bryanvincentrferreira@gmail.com>
2025-09-22 23:01:11 -0300

    Subindo automação com typescript na plataforma QA xperience

---
+++ loginxp-pwts/tests/login.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect } from '@playwright/test'
+import { Account, Login } from './support/actions/login'
+
+let login: Login
+
+test.beforeEach(({page}) => {
+  login = new Login(page)
+})
+
+test('deve logar com sucesso', async ({ page }) => {
+  const account: Account = {
+    username: 'qa',
+    password: 'xperience'
+  }
+  await login.submit(account)
+  await expect(
+    await login.getPopUpContent()
+    ).toContainText('Suas credenciais são válidas :)')
+})
+
+test('Não deve logar com senha inválida', async ({ page }) => {
+  const account: Account = {
+    username: 'qa',
+    password: '12346'
+  }
+  await login.submit(account)
+  await login.assertToast('Oops! Credenciais inválidas :(')
+})
+
+test('Não deve logar quando não é preenchido todos os campos', async ({ page }) => {
+  const account: Account = {
+    username: '',
+    password: ''
+  }
+  await login.submit(account)
+  await login.assertToast('Informe o seu nome de usuário!')
+})
+
+test('Não deve logar quando não informo a senha', async ({ page }) => {
+  const account: Account = {
+    username: 'qa',
+    password: ''
+  }
+  await login.submit(account)
+  await login.assertToast('Informe a sua senha secreta!')
+})
+
+test('Não deve logar quando não informo o usuário', async ({ page }) => {
+  const account: Account = {
+    username: '',
+    password: 'xperience'
+  }
+  await login.submit(account)
+  await login.assertToast('Informe o seu nome de usuário!')
+})
+
